49330adcb6678f1394ca3b17e1f658d4cd57fa9e
Zoran Topic <zoran1607@gmail.com>
2022-02-11 11:31:19 +0100

    minor change

--- exercise/test.py
+++ exercise/test.py
@@ -1,1 +1,12 @@
-.
+class Junior
+    ''' This could be me '''
+    def __init__(self):
+        self.name
+        self.age
+        self.ambition
+        self.persistence
+        self.skill
+
+    def print_format(self):
+        ''' prints neatly formated attributes '''
+        print(f'')
