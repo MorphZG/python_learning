771e5559fb34129f95678bdbc3ba94a9d4a44397
Zoran Topic <zoran1607@gmail.com>
2022-02-21 13:34:21 +0100

    day 21 completed

--- 100_days_of_code/20_21_Snake_game_p1_Animation_and_Coordinates_p2_Inheritance_and_Slicing/snake.py
+++ 100_days_of_code/20_21_Snake_game_p1_Animation_and_Coordinates_p2_Inheritance_and_Slicing/snake.py
@@ -10,10 +10,10 @@ RIGHT = 0  # heading toward East
 
 
 class Snake:
-    """ creates a compact snake out of square turtles """
+    ''' creates a compact snake out of square turtles '''
 
     def __init__(self):
-        """ initialize the snake """
+        ''' initialize the snake '''
 
         self.snake_segments = []
         self.create_snake()
@@ -21,22 +21,23 @@ class Snake:
         # snake_segment[0] is setting the position for the rest of body
         # will call it "head" to make it readable
         self.head = self.snake_segments[0]
+        # to extend the snake, add to last snake_segment
+        # will call it "tail" to make it readable
+        self.tail = self.snake_segments[-1]
 
     def create_snake(self):
-        """ creates the snake segment by segment, auto call on __init__"""
-
+        '''
+        add snake segment at each position in STARTING_POSITIONS
+        auto call on __init__
+        '''
         for position in STARTING_POSITIONS:
-            square = Turtle(shape='square')
-            square.penup()  #               <! --- penup()
-            square.color('white')
-            square.goto(position)
-            self.snake_segments.append(square)
+            self.add_segment(position)
 
     def move(self):
-        """
+        '''
         let the snake continously move forward
         body follows the head by taking position of segment in front
-        """
+        '''
         for i in range(len(self.snake_segments)-1, 0, -1):  # loop from last to first index, reverse
             xpos = self.snake_segments[i - 1].xcor()  # position of middle square
             ypos = self.snake_segments[i - 1].ycor()  # position of middle square
@@ -44,23 +45,40 @@ class Snake:
 
         self.head.forward(MOVE_DISTANCE)
 
+    def add_segment(self, position):
+        ''' add a white square segment to snake '''
+        square = Turtle(shape='square')
+        square.color('white')
+        square.penup()
+        square.goto(position)
+        self.snake_segments.append(square)
+
+    def extend_snake(self):
+        '''
+        call add_segment(position) where position is
+        position() of self.tail or last item in snake_segments
+        '''
+        self.add_segment(self.tail.position())
+
+
+# define the movement methods
     def up(self):
-        """ set the snake heading North """
+        ''' set the snake heading North '''
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
 
     def down(self):
-        """ set the snake heading South """
+        ''' set the snake heading South '''
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
 
     def left(self):
-        """ set the snake heading West """
+        ''' set the snake heading West '''
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
 
     def right(self):
-        """ set the snake heading East """
+        ''' set the snake heading East '''
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
 
